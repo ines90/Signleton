98b0e0582a3f1d695c831f9994f0a9e302b70fac
ines90 <agnieszka.ptach@gmail.com>
2018-02-18 12:58:11 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+
+        System.out.println(a == b);
+    }
+
+}
